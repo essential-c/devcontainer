9df12aa81d90d6025ad15c5d63f46a99f0d7a0aa
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2026-08-06 18:41:33 +0100

    sync atoi.c with the book

--- 02-basics/atoi.c
+++ 02-basics/atoi.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Sum the two integers passed as command line integers */
+// Sum the two integers passed as command line parameters
 int main(int argc, char *argv[]) {
     int a, b;
 
     // Once again, dangerous!
-    // We'll fix that in the next lecture regarding control flow
     a = atoi(argv[1]);
     b = atoi(argv[2]);
 
